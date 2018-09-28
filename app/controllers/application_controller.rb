89f98e5c5ad562feb25ed64353db2e60cd9ec610
Scott Weinstein <sweinstein27@gmail.com>
2018-09-28 16:01:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def hello
+    redirect_to controller: 'sessions', action: 'new' unless session[:name]
+  end
 
   def current_user
-    @current_user ||= session[:name] if session[:name]
+    session[:name]
   end
+
+  private
+
+  def require_logged_in
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end
+
 
 end
